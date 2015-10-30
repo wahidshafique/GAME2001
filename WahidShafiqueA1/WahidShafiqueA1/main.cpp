de9fcacc209063298ce77b3b0f33680f3c3611f1
wahidshafique <wahidshafique@hotmail.com>
2015-10-29 20:23:17 -0400

    fixed index error, finalized

--- WahidShafiqueA1/WahidShafiqueA1/main.cpp
+++ WahidShafiqueA1/WahidShafiqueA1/main.cpp
@@ -13,14 +13,13 @@ BitArray genGrid(unsigned int rows, unsigned int cols, unsigned int percentage) 
 	grid.ClearAllBits();
 	for (unsigned int i = 0; i < gridLen; i++) {
 		randNum = rand() % 100 + 1;
-		(randNum <= percentage) ? grid.SetBit(i) : grid.ClearBit(i);
+		(randNum < percentage) ? grid.SetBit(i) : grid.ClearBit(i);
 	}
 	return grid;
 }
 
 string gridView(BitArray &grid, unsigned int rows, unsigned int cols) {
 	string gridString;
-
 	for (unsigned int i = 0; i < rows; i++) {
 		for (unsigned int k = 0; k < cols; k++) {
 			static unsigned int index = 0;
@@ -38,65 +37,55 @@ void markBlob(BitArray &grid, BitArray &visited, unsigned int rows, unsigned int
 	if (!visited[gridIndexer] && grid[gridIndexer]) {
 		visited.SetBit(gridIndexer);
 
-		if (col < cols - 1) {
-			if (grid[gridIndexer + 1]) {
-				markBlob(grid, visited, rows, cols, row, col + 1);
-			}
+		if (row > 0 && grid[gridIndexer - cols]) {
+			markBlob(grid, visited, rows, cols, row - 1, col);
 		}
-		if (col > 0) {
-			if (grid[gridIndexer - 1]) {
-				markBlob(grid, visited, rows, cols, row, col - 1);
-			}
+		if (row + 1 < rows && grid[gridIndexer + cols]) {
+			markBlob(grid, visited, rows, cols, row + 1, col);
 		}
-
-		if (row < rows - 1 ) {
-			if (grid[gridIndexer + col]) {
-				markBlob(grid, visited, rows, cols, row + 1, col);
-			}
+		if (col > 0 && grid[gridIndexer - 1]) {
+			markBlob(grid, visited, rows, cols, row, col - 1);
 		}
-		if (row > 0) {
-			if (grid[gridIndexer - col]) {
-				markBlob(grid, visited, rows, cols, row - 1, col);
-			}
-
+		if (col + 1 < cols && grid[gridIndexer + 1]) {
+			markBlob(grid, visited, rows, cols, row, col + 1);
 		}
 	}
 }
 
-	int blobCount(BitArray &grid, BitArray& visited, unsigned int rows, unsigned int cols) {
-		int counter = 0;
-		visited.ClearAllBits();
+int blobCount(BitArray &grid, BitArray &visited, unsigned int rows, unsigned int cols) {
+	int counter = 0;
+	visited.ClearAllBits();
 
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (grid[j + (i * cols)] && !visited[j + (i * cols)]) {
-					markBlob(grid, visited, rows, cols, i, j);
-					counter++;
-				}
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (grid[j + (i * cols)] && !visited[j + (i * cols)]) {
+				counter++;
+				markBlob(grid, visited, rows, cols, i, j);
 			}
 		}
-		return counter;
 	}
+	return counter;
+}
 
-	int main() {
-		int rows;
-		int cols;
-		int percent;
-		string welcome = "Blob Counter By: Wahid Shafique";
+int main() {
+	int rows;
+	int cols;
+	int percent;
+	string welcome = "Blob Counter By: Wahid Shafique";
 
-		cout << welcome << endl;
-		cout << string(welcome.length(), '*') << endl;
-		cout << "Enter number of rows: ";
-		cin >> rows;
-		cout << "Enter number of cols: ";
-		cin >> cols;
-		cout << "Enter blob percentage: ";
-		cin >> percent;
+	cout << welcome << endl;
+	cout << string(welcome.length(), '*') << endl;
+	cout << "Enter number of rows: ";
+	cin >> rows;
+	cout << "Enter number of cols: ";
+	cin >> cols;
+	cout << "Enter blob percentage: ";
+	cin >> percent;
 
-		if ((rows * cols) != 0) {
-			BitArray grid = genGrid(rows, cols, percent);
-			BitArray visited = genGrid(rows, cols, 0);
-			cout << gridView(grid, rows, cols) << endl;
-			cout << blobCount(grid, visited, rows, cols) << endl;
-		}
+	if ((rows * cols) != 0) {
+		BitArray grid = genGrid(rows, cols, percent);
+		BitArray visited = genGrid(rows, cols, 0);
+		cout << gridView(grid, rows, cols) << endl;
+		cout << blobCount(grid, visited, rows, cols) << endl;
 	}
+}
